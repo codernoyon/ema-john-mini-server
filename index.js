da94821353f30c77a87e6dc9f3af067e3938c63b
codernoyon <noyonrahman2003@gmail.com>
2022-05-01 10:16:52 +0600

    pagination api added:

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 
@@ -8,6 +9,52 @@ const app = express();
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mzocn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+
+        app.get('/products', async(req, res) =>{
+            // console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            
+            if(page || size){
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }else{
+                products = await cursor.toArray();
+            }
+            
+            res.send(products);
+        });
+
+        app.get('/productCount', async (req, res) => {
+            // const query = {};
+            // const cursor = productCollection.find(query)
+            // const count = await cursor.count();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count})
+        }) 
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
+
+
 
 
 // testing server api
